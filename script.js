66c5162d7ca917fc1a102437f00aa0480c9994e1
vignesh109 <vignesh109@gmail.com>
2017-06-09 17:03:21 +0530

    Create script.js

--- script.js
+++ script.js
@@ -53,10 +53,10 @@ alert(1);
 		} else if(actualLength == maxLength && newLength >= 2){
 			$(this).val(0);
 		} else if(newLength == 0){
-//			alert('backspace');
+// 			alert('backspace');
 			originalVal1 = originalVal.slice(0, -1);
 			originalVal2 = originalVal.substring(0, originalVal.length - 1);
-			//alert(originalVal1+" and "+originalVal2);
+			alert(originalVal1+" and "+originalVal2);
 			$("#" + idPrefix).val(originalVal2);
 			$(this).val(0);
 		}
