7c164b5a0b09320484b8c44c5bea3898ac02945f
vignesh109 <vignesh109@gmail.com>
2017-06-09 16:09:08 +0530

    Create script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ alert(3);
 			newVal1 = newVal.slice(0, -1);
 			newVal2 = myVal.substring(0, myVal.length - 1);
 			alert(newVal1+" and "+newVal2);
-			$(this).val(0);
+			$(this).val(newVal1);
 		} else if(actualLength == maxLength && newLength >= 2){
 			$(this).val(0);
 		} else if(newLength == 0){
@@ -58,7 +58,7 @@ alert(3);
 			originalVal1 = originalVal.slice(0, -1);
 			originalVal2 = originalVal.substring(0, originalVal.length - 1);
 			alert(originalVal1+" and "+originalVal2);
-			$("#" + idPrefix).val(originalVal)
+			$("#" + idPrefix).val(originalVal2)
 			$(this).val(0);
 		}
 	});
