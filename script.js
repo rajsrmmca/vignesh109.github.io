64240afedbbf13e69fddd60495951233b3abfb25
rajsrmmca <rajsrmmca@gmail.com>
2017-06-14 15:48:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,169 @@
-// Add your javascript here
-$(function() {
-	$(".changeKeyType").on('click', function(e) {
-	    $("#hiddenInput").val(0);
-	    var button = $(this);
-	    idPrefix = (this.id).split('-')[0];
-	    var inputsuffix = button.text().trim();
-	    var toggleText = (inputsuffix == '123' ? 'abc' : '123');
-	    var toggleType = (inputsuffix == '123' ? 'number' : 'text');
-	    $("#hiddenInput").attr('type', toggleType);
-	    originalVal = $("#" + idPrefix).val();
-	    $(this).text(toggleText);
-	    oldLength = 0;
-	    if ($("#hiddenInput").val() !== undefined) {
-	        oldLength = $("#hiddenInput").val().length;
-	    }
-	    $("#" + idPrefix).addClass("activeInput");
-	    $("#" + idPrefix).on('focus', function() {
-	        button.text('abc');
-	        $("#hiddenInput").attr('type', 'number');
-	    });
-	
-	    maxLength = $("#" + idPrefix).attr("maxlength");
-	    $("#hiddenInput").focus();
-	});
-	
-	$(".changeKeyInput").on('click focus', function() {
-	    $("#hiddenInput").focus().val(0);
-	    $("#hiddenInput").attr('type', 'number');
-	    idPrefix = (this.id).split('-')[0];
-	    originalVal = $(this).val();
-	    oldLength = 0;
-	    if ($("#hiddenInput").val() !== undefined) {
-	        oldLength = $("#hiddenInput").val().length;
-	    }
-	    maxLength = $(this).attr("maxlength");
-	});
+var numButtons = "";
+    var inputToggle,sel;
+    //add numbers 0-9
 
- $("#hiddenInput").off().on('keyup', function(event) {
-		var actualLength = $("#" + idPrefix).val().length;
-		var newVal = $(this).val();
-		var newLength = newVal.length;
-		if (actualLength < maxLength && newLength == 2) {
-			originalVal += newVal[newLength - 1];
-			$("#" + idPrefix).val(originalVal);
-			newVal = newVal.slice(0, -1);
-			$(this).val(0);
-		} else if(actualLength == maxLength && newLength >= 2){
-			$(this).val(0);
-		} else if(newLength == 0){
-//			alert('backspace');
-			originalVal = originalVal.slice(0, -1);
-			$("#" + idPrefix).val(originalVal);
-			$(this).val(0);
-		}
-	});
+        numButtons += "<div class='row' style='margin-left:0px;'><div class='col-xs-4' style='text-align: center;'><button class='numKey' value='1' onclick='addValueToField(this.value)'  />" + 1 + "</button></div>"
+    numButtons += "<div class='col-xs-4' style='text-align: center;'><button class='numKey' value='2' onclick='addValueToField(this.value)'  />" + 2 + "</button></div>"
+    numButtons += "<div class='col-xs-4' style='text-align: center;'><button class='numKey' value='3' onclick='addValueToField(this.value)'  />" + 3 + "</button></div>"
+
+    numButtons += "<div class='col-xs-4' style='text-align: center;'><button class='numKey' value='4' onclick='addValueToField(this.value)'  />" + 4 + "</button></div>"
+    numButtons += "<div class='col-xs-4' style='text-align: center;'><button class='numKey' value='5' onclick='addValueToField(this.value)'  />" + 5 + "</button></div>"
+    numButtons += "<div class='col-xs-4' style='text-align: center;'><button class='numKey' value='6' onclick='addValueToField(this.value)' />" + 6 + "</button></div>"
+
+    numButtons += "<div class='col-xs-4' style='text-align: center;'><button class='numKey' value='7' onclick='addValueToField(this.value)'  />" + 7 + "</button></div>"
+    numButtons += "<div class='col-xs-4' style='text-align: center;'><button class='numKey' value='8' onclick='addValueToField(this.value)'  />" + 8 + "</button></div>"
+    numButtons += "<div class='col-xs-4' style='text-align: center;'><button class='numKey' value='9' onclick='addValueToField(this.value)'  />" + 9 + "</button></div>"
+    numButtons += "<div class='col-xs-4' style='text-align: center;'><button class='numKey' value='abc' onclick='addValueToField(this.value)'  /> abc </button></div>"
+
+    numButtons += "<div class='col-xs-4' style='text-align: center;'><button class='numKey' value='0' onclick='addValueToField(this.value)' >0</button></div><div class='col-xs-4' style='text-align: center;'> <button class='numKey' value='<' onclick='addValueToField(this.value)' ><</button></div> "
+    numButtons += "<div class='col-xs-6 col-xs-offset-3' style='text-align: center;'><button class='numKey' value='GO' onclick='addValueToField(this.value)'  /> GO </button></div></div>"
+    document.getElementById('switch').innerHTML = numButtons;
+    function hideKeyboard(element) {
+        $(element).attr('readonly', 'readonly'); // Force keyboard to hide on input field.
+
+        setTimeout(function() {
+            $(element).blur();  //actually close the keyboard
+            document.activeElement.blur();
+            $('input').blur(); // trigger blur event from all inputs.
+            // Remove readonly attribute after keyboard is hidden.
+            $(element).removeAttr('readonly');
+           // $(element).focus(); //bring cursor back to the input field
+        }, 100);
+
+    }
+    function addValueToField(myValue){
+        console.log(inputToggle);
+        //to bring up alphabet keyboard
+        if(myValue=='abc')
+        {
+            document.getElementById(inputToggle).focus();
+           // document.getElementById("trailerNumber").onfocus=null;
+            document.getElementById("switch").style.display = "none";
+            $(document.getElementById(inputToggle)).removeAttr('readonly');
+
+        }
+        else if(myValue=='GO')
+        {
+            $('.gateIn').click();
+        }
+        else if (myValue=='<') {
+            hideKeyboard(document.getElementById(inputToggle));
+
+                if (document.selection) {
+                    //For browsers like Internet Explorer
+
+                    document.getElementById(inputToggle).focus();
+                    sel = document.selection.createRange();
+                    sel.text = myValue;
+                   // document.getElementById(inputToggle).focus();
+                }
+                else if (document.getElementById(inputToggle).selectionStart || document.getElementById(inputToggle).selectionStart == '0') {
+                    //For browsers like Firefox and Webkit based
+                    var startPos = document.getElementById(inputToggle).selectionStart;
+                    var endPos = document.getElementById(inputToggle).selectionEnd;
+                    var scrollTop = document.getElementById(inputToggle).scrollTop;
+                    document.getElementById(inputToggle).value = document.getElementById(inputToggle).value.substring(0, startPos - 1) + document.getElementById(inputToggle).value.substring(endPos, document.getElementById(inputToggle).value.length);
+                    document.getElementById(inputToggle).focus();
+                    document.getElementById(inputToggle).selectionStart = startPos - myValue.length;
+                    document.getElementById(inputToggle).selectionEnd = startPos - myValue.length;
+                    document.getElementById(inputToggle).scrollTop = scrollTop;
+                } else {
+                    //  document.getElementById(inputToggle).value += myValue;
+                    document.getElementById(inputToggle).focus();
+                }
+                document.getElementById("switch").style.display = "flex";
+
+
+        }
+        else {
+            hideKeyboard(document.getElementById(inputToggle));
+
+        if (document.selection) {
+            //For browsers like Internet Explorer
+            document.getElementById(inputToggle).focus();
+            sel = document.selection.createRange();
+            sel.text = myValue;
+            document.getElementById(inputToggle).focus();
+        }
+        else if (document.getElementById(inputToggle).selectionStart || document.getElementById(inputToggle).selectionStart == '0') {
+            //For browsers like Firefox and Webkit based
+            var startPos = document.getElementById(inputToggle).selectionStart;
+            var endPos = document.getElementById(inputToggle).selectionEnd;
+            var scrollTop = document.getElementById(inputToggle).scrollTop;
+            document.getElementById(inputToggle).value = document.getElementById(inputToggle).value.substring(0, startPos)+myValue+document.getElementById(inputToggle).value.substring(endPos,document.getElementById(inputToggle).value.length);
+            document.getElementById(inputToggle).focus();
+            document.getElementById(inputToggle).selectionStart = startPos + myValue.length;
+            document.getElementById(inputToggle).selectionEnd = startPos + myValue.length;
+            document.getElementById(inputToggle).scrollTop = scrollTop;
+        } else {
+            document.getElementById(inputToggle).value += myValue;
+            document.getElementById(inputToggle).focus();
+        }
 
 
 
+        // document.getElementById(inputToggle).focus();
+        document.getElementById("switch").style.display = "flex";
+          }
+        }
 
-});
+
+        document.addEventListener('mousedown', function(e) {
+            e = e || window.event;
+            var target = e.target || e.srcElement;
+
+            console.log(e);
+            if(target.id == 'trailerNumber' ){
+
+                hideKeyboard(document.getElementById("trailerNumber"));
+
+                //document.getElementById("trailerNumber").blur();
+                setTimeout(function() {
+                    document.getElementById("trailerNumber").focus();
+                    inputToggle = 'trailerNumber';
+
+                    document.getElementById("switch").style.display = "flex";
+                },400);
+
+            }
+            else if(target.id == 'inboundNumber' ){
+                document.getElementById("switch").style.display = "flex";
+                document.getElementById("inboundNumber").focus();
+
+                inputToggle='inboundNumber';
+            }
+
+            else if(target.id == 'trailerIdLabel' ){
+                document.getElementById("switch").style.display = "flex";
+                document.getElementById("trailerIdLabel").focus();
+                inputToggle='trailerIdLabel';
+            }
+
+            else if(target.id == 'inboundSeal' ){
+                document.getElementById("switch").style.display = "flex";
+                document.getElementById("inboundSeal").focus();
+                inputToggle='inboundSeal';
+            }
+            else if(target.id == 'tractor' ){
+                document.getElementById("switch").style.display = "flex";
+                document.getElementById("tractor").focus();
+                inputToggle='tractor';
+            }
+            else if(target.id == 'newSeal' ){
+                document.getElementById("switch").style.display = "flex";
+                document.getElementById("newSeal").focus();
+                inputToggle='newSeal';
+            }
+            else if(( target.id == "switch" || target.className == "numKey") && target.value != 'abc'){
+
+                document.getElementById("switch").style.display = "flex";
+                console.log('clicked switch');
+            }
+            else{
+                document.getElementById("switch").style.display = "none";
+                //document.getElementById("trailerNumber").blur();
+            }
+            console.log((document.getElementById("trailerNumber")).value.length);
+
+
+    }, false);
